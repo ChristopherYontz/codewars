3cf605b6894dba2076e52b446ae8d56fa21bd1de
Christopher Yontz <yontzcay@gmail.com>
2023-08-09 22:48:48 -0700

    'Complete new Codewars challenge'

--- beginner-series-#2-clock.js
+++ beginner-series-#2-clock.js
@@ -13,6 +13,14 @@
 
 // MY SOLUTION
 function past(h, m, s) {
-    const currentTime = new Date();
+    let hoursInMilliseconds = h * 60 * 60 * 1000;
+    let minutesInMilliseconds = m * 60 * 1000;
+    let secondsInMilliseconds = s * 1000; 
     
+    return hoursInMilliseconds + minutesInMilliseconds + secondsInMilliseconds;
 }
+
+
+
+// OTHER SOLUTIONS
+let past = (h, m, s) => ((h * 3600) + (m * 60) + s) * 1000;
