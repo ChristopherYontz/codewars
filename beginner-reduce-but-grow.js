a9b0d62450a58e0949feb260f3030d9f34d7aac7
Christopher Yontz <yontzcay@gmail.com>
2023-08-14 19:38:15 -0700

    'Complete new Codewars challenge'

---
+++ beginner-reduce-but-grow.js
@@ -0,0 +1,6 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+// MY SOLUTION
+let grow = x => x.reduce((a, c) => a * c, 0);
