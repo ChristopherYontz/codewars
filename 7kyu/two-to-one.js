3c67cdb4e2c9e1c808ffd4bf115da0fc55bdc0f6
Christopher Yontz <yontzcay@gmail.com>
2023-11-02 00:05:47 -0700

    Complete new Codewars challenge

--- 7kyu/two-to-one.js
+++ 7kyu/two-to-one.js
@@ -10,9 +10,18 @@ a = "abcdefghijklmnopqrstuvwxyz"
 longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 */
 
+// MY SOLUTION
 function longest (s1, s2) {
     const combined = [...s1].concat([...s2])
-    const filtered = combined.filter((e, i) => e != combined[i - 1])
-    const sorted = filtered.sort().join('')
-    return sorted
+    const filtered = []
+    for (i = 0; i < combined.length; i ++) {
+        if (!filtered.includes(combined[i])) {
+            filtered.push(combined[i])
+        }
+    }
+    return filtered.sort().join('')
 }
+
+// OTHER SOLUTIONS
+const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join('')
+    // Sets can only contain distinct (unique) values
